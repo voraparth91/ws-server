814983bf6b7dc424b0da948483c9b89ca6865b1e
Parth Vora <parth.vora@Parth-Vora-C02ZQ0A4MD6M.local>
2024-05-16 21:32:46 +0530

    Changing the port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 80 });
 
 wss.on('connection', (ws) => {
     console.log('New client connected');

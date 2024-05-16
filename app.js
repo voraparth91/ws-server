808cdc4d6657aaf3a31419b5512abe4eca306b75
Parth Vora <parth.vora@Parth-Vora-C02ZQ0A4MD6M.local>
2024-05-16 21:13:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+
+        // Broadcast the received message to all other clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message.toString()); // Ensure message is sent as a text string
+                console.log('Sent message to other client');
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+console.log('Signaling server running on ws://localhost:8080');
